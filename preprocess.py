c7924ba6a27d9126ddaf5d81399a3d3a41227a51
Sagnik Chakraborty <143476306+Sagnik-23@users.noreply.github.com>
2025-07-24 19:18:47 +0530

    All User solved problem

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import re
 import pandas as pd
 def preprocess(data):
     print("Preprocessing data...")
-    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\u202f[AP]M\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}(?:\s?[APMapm]{2})?\s-\s'
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
     dates = [d.replace('\u202f', ' ') for d in dates]
